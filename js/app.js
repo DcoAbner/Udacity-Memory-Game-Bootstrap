012ee5b736295a7309ef62e16be835dc6ece7d69
Travis Henry <travis.s.henry@gmail.com>
2017-09-06 15:02:16 -0700

    Added shuffle to cards

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ function createDeck(cards) {
             cardArray.push(card);
         }
     }
-    return cardArray;
+    return shuffle(cardArray);
 }
 
 let deckOfCards = [];
@@ -128,3 +128,18 @@ function drawCards() {
     }
 }
 
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
