9df45c87b997c47df1e078f83187ff34656d6ec2
Travis Henry <travis.s.henry@gmail.com>
2017-09-06 14:50:53 -0700

    Added matching logic and card counter to win game

--- js/app.js
+++ js/app.js
@@ -9,12 +9,12 @@ let listOfCards = [
     {
         icon: 'bomb'
     },
-    {
-        icon: 'leaf'
-    },
-    {
-        icon: 'bolt'
-    }
+    // {
+    //     icon: 'leaf'
+    // },
+    // {
+    //     icon: 'bolt'
+    // }
 ]
 
 //function creates two copies of each card and returns the new array
@@ -29,6 +29,8 @@ function createDeck(cards) {
 }
 
 let deckOfCards = [];
+let numberOfCards = 0;
+let numberOfMatchedCards = 0;
 
 selectedCard1 = null;
 selectedCard2 = null;
@@ -36,8 +38,10 @@ selectedCard2 = null;
 $(document).ready( function() {
 
     deckOfCards = createDeck(listOfCards);
+    numberOfCards = deckOfCards.length;
+    numberOfMatchedCards = 0;
 
-    for (let i=0; i<deckOfCards.length; i++) {
+    for (let i=0; i<numberOfCards; i++) {
         $("#cardTable").append(`<div data-cardPosition=${i} class="card col-md-4 col-lg-3 col-sm-6"></div>`);
     }
 
@@ -45,41 +49,54 @@ $(document).ready( function() {
 
 $("#cardTable").on('click', 'div', function() {
 
-    if (!selectedCard1) {
-        selectedCard1 = $(this).attr('data-cardPosition');
-        displayIcon(selectedCard1);
-    } else if (!selectedCard2) {
-        if (selectedCard1 !== $(this).attr('data-cardPosition')) {
-            selectedCard2 = $(this).attr('data-cardPosition');
-            displayIcon(selectedCard2);
+    if ($(this).hasClass("open")) {
+        return;
+    } else {
+        console.log("click");
+        if (!selectedCard1) {
+            selectedCard1 = $(this).attr('data-cardPosition');
+            displayIcon(selectedCard1);
+        } else if (!selectedCard2) {
+            if (selectedCard1 !== $(this).attr('data-cardPosition')) {
+                selectedCard2 = $(this).attr('data-cardPosition');
+                displayIcon(selectedCard2);
 
-            if (!compareCards(selectedCard1, selectedCard2)) {
-                hideIcon(selectedCard1);
-                hideIcon(selectedCard2);
+                if (!compareCards(selectedCard1, selectedCard2)) {
+                    hideIcon(selectedCard1);
+                    hideIcon(selectedCard2);
+                } else {
+                    if (numberOfMatchedCards === numberOfCards) {
+
+                        endGame();
+
+                    }
+                }
+
+                selectedCard1 = null;
+                selectedCard2 = null;
+
             }
-
-            selectedCard1 = null;
-            selectedCard2 = null;
-
         }
-
-
     }
-
 })
 
 function compareCards(card1Index, card2Index) {
     if (deckOfCards[card1Index].icon === deckOfCards[card2Index].icon) {
         console.log("Match");
+        $($("#cardTable").children()[card1Index]).addClass("matched");
+        $($("#cardTable").children()[card2Index]).addClass("matched");
+        numberOfMatchedCards += 2;
         return true;
     } else {
         console.log("No")
+        return false;
     }
 }
 
 function displayIcon(cardIndex) {
 
     let currentCard = $("#cardTable").children()[cardIndex];
+    $(currentCard).addClass("open");
     $(currentCard).append(`<i class="fa fa-${deckOfCards[cardIndex].icon}"></i>`);
 
 }
@@ -87,10 +104,13 @@ function displayIcon(cardIndex) {
 function hideIcon(cardIndex) {
 
     let currentCard = $("#cardTable").children()[cardIndex];
-    console.log(currentCard);
+    $(currentCard).removeClass("open");
     $(currentCard).children('i').remove();
 
 }
 
+function endGame() {
+    alert("You win");
+}
 
 
